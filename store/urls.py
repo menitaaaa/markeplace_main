68a6c744ece7fb6313909021e584dce8304654c8
menitaaaa <serranoricodulcejimena@gmail.com>
2026-05-26 19:37:52 -0700

    modifique

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('cart/', views.cart_detail, name='cart_detail'),
+    path('cart/add/<uuid:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
