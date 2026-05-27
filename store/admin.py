634a775f1bb519a1729448935bd9bb63d473a932
menitaaaa <serranoricodulcejimena@gmail.com>
2026-05-26 19:31:01 -0700

    guarde

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User, Category, Product, Cart, CartItem
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_seller')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock', 'owner')
+
+admin.site.register(Cart)
+admin.site.register(CartItem)
