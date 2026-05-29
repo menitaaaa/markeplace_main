36b6bd492f68e08c50eea22a63264101d05e74c1
menitaaaa <serranoricodulcejimena@gmail.com>
2026-05-28 18:46:08 -0700

    modifique

--- store/admin.py
+++ store/admin.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from .models import User, Category, Product, Cart, CartItem
 
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_seller')
-
+# Configuración para Categorías
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
+# Configuración para Productos
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock', 'owner')
+    # Solo ponemos nombre, precio y stock para que no falle
+    list_display = ('name', 'price', 'stock')
 
+# Registros simples para el resto de modelos
+admin.site.register(User)
 admin.site.register(Cart)
 admin.site.register(CartItem)
